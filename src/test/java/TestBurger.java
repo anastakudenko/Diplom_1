17a2f9438f31c6e31bb3e94cf37fe4e2b61c84b5
Nastya Kudenko <kudya02@bk.ru>
2023-06-16 10:01:02 +0300

    small fix

--- src/test/java/TestBurger.java
+++ src/test/java/TestBurger.java
@@ -1,88 +1,104 @@
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Bun;
 import praktikum.Burger;
 import praktikum.Ingredient;
 import praktikum.IngredientType;
-
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class TestBurger {
+    private Burger blackBurger;
+
+    public Bun getBunMock() {
+        Bun createdBunMock = Mockito.mock(Bun.class);
+        Mockito.when(createdBunMock.getPrice()).thenReturn(0.5f);
+        Mockito.when(createdBunMock.getName()).thenReturn("беленькая");
+        return createdBunMock;
+    }
+
+    public Ingredient getFirstIngredientMock() {
+        Ingredient createdIngredientMock = Mockito.mock(Ingredient.class);
+        Mockito.when(createdIngredientMock.getName()).thenReturn("мягкая");
+        Mockito.when(createdIngredientMock.getType()).thenReturn(IngredientType.FILLING);
+        Mockito.when(createdIngredientMock.getPrice()).thenReturn(10.43f);
+        return createdIngredientMock;
+    }
+
+    public Ingredient getSecondIngredientMock() {
+        Ingredient createdIngredientMock = Mockito.mock(Ingredient.class);
+        Mockito.when(createdIngredientMock.getName()).thenReturn("жиденький");
+        Mockito.when(createdIngredientMock.getType()).thenReturn(IngredientType.SAUCE);
+        Mockito.when(createdIngredientMock.getPrice()).thenReturn(0.88f);
+        return createdIngredientMock;
+    }
+
+    @Before
+    public void setUp() {
+        blackBurger = new Burger();
+    }
+
     @Test
     public void testSetBun() {
-        Burger blackBurger = new Burger();
-        String nameBun = "беленькая";
-        float priceBun = 22.8F;
-        Bun whiteBun = new Bun(nameBun, priceBun);
+        Bun whiteBun = getBunMock();
         blackBurger.setBuns(whiteBun);
-        assertEquals(whiteBun, blackBurger.bun);
+        assertEquals("Булочки не сопадают", whiteBun, blackBurger.bun);
     }
 
     @Test
     public void testAddIngredient() {
-        Burger blackBurger = new Burger();
-        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "жиденький", 0.1F);
+        Ingredient ingredient = getFirstIngredientMock();
         blackBurger.addIngredient(ingredient);
-        assertTrue(blackBurger.ingredients.contains(ingredient));
+        assertTrue("Ингредиент не добавлен", blackBurger.ingredients.contains(ingredient));
     }
 
     @Test
     public void testRemoveIngredient() {
-        Burger blackBurger = new Burger();
-        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "жиденький", 0.1F);
+        Ingredient ingredient = getFirstIngredientMock();
         blackBurger.addIngredient(ingredient);
         blackBurger.removeIngredient(0);
-        assertFalse(blackBurger.ingredients.contains(ingredient));
+        assertFalse("Ингредиент не удален", blackBurger.ingredients.contains(ingredient));
     }
 
     @Test
     public void testMoveIngredient() {
-        Burger blackBurger = new Burger();
-        Ingredient ingredient1 = new Ingredient(IngredientType.SAUCE, "жиденький", 0.1F);
-        Ingredient ingredient2 = new Ingredient(IngredientType.FILLING, "тверденький", 1.5F);
+        Ingredient ingredient1 = getFirstIngredientMock();
+        Ingredient ingredient2 = getSecondIngredientMock();
         blackBurger.addIngredient(ingredient1);
-        blackBurger.addIngredient(ingredient2);
-        assertEquals(blackBurger.ingredients.get(0), ingredient1);
-        assertEquals(blackBurger.ingredients.get(1), ingredient2);
+        blackBurger.addIngredient(ingredient1);
         blackBurger.moveIngredient(0, 1);
-        assertEquals(blackBurger.ingredients.get(0), ingredient2);
-        assertEquals(blackBurger.ingredients.get(1), ingredient1);
+        assertEquals("Ингредиент не переместился", ingredient1, blackBurger.ingredients.get(1));
+
     }
     @Test
     public void testGetPrice() {
-        Burger blackBurger = new Burger();
-        float priceIng1 = 0.1F;
-        float priceIng2 = 1.5F;
-        float priceBun = 22.8F;
-        Ingredient ingredient1 = new Ingredient(IngredientType.SAUCE, "жиденький",  priceIng1);
-        Ingredient ingredient2 = new Ingredient(IngredientType.FILLING, "тверденький", priceIng2);
-        Bun whiteBun = new Bun("тепленькая", priceBun);
+        Bun whiteBun = getBunMock();
+        Ingredient ingredient1 = getFirstIngredientMock();
+        Ingredient ingredient2 = getSecondIngredientMock();
         blackBurger.setBuns(whiteBun);
         blackBurger.addIngredient(ingredient1);
         blackBurger.addIngredient(ingredient2);
-        float expectedPrice = priceBun * 2 + priceIng2 + priceIng1;
-        assertEquals(expectedPrice,blackBurger.getPrice(),0.0F);
+        float expectedPrice = whiteBun.getPrice() * 2 + ingredient1.getPrice() + ingredient2.getPrice();
+        assertEquals("Стоимость не совпадает",expectedPrice,blackBurger.getPrice(),0.0F);
     }
 
     @Test
     public void testGetReceipt() {
-        Burger blackBurger = new Burger();
-        Ingredient ingredient1 = new Ingredient(IngredientType.SAUCE, "жиденький",  0.1F);
-        Ingredient ingredient2 = new Ingredient(IngredientType.FILLING, "тверденький", 1.1F);
-        Bun whiteBun = new Bun("тепленькая", 0.5F);
+        Bun whiteBun = getBunMock();
+        Ingredient ingredient1 = getFirstIngredientMock();
+        Ingredient ingredient2 = getSecondIngredientMock();
         blackBurger.setBuns(whiteBun);
         blackBurger.addIngredient(ingredient1);
         blackBurger.addIngredient(ingredient2);
-        System.out.println(blackBurger.getReceipt());
-        String expectedReceipt = "(==== тепленькая ====)\n" +
+        String expectedReceipt = "(==== беленькая ====)\n" +
+                "= filling мягкая =\n" +
                 "= sauce жиденький =\n" +
-                "= filling тверденький =\n" +
-                "(==== тепленькая ====)\n" +
+                "(==== беленькая ====)\n" +
                 "\n" +
-                "Price: 2,200000\n";
+                "Price: 12,310000\n";
         assertEquals(blackBurger.getReceipt(), expectedReceipt);
     }
 }
